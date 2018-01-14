9a1c59a522f8eb1e5052ed36d467f3a79305065a
Rhuan Santos <rhuansantosdev@gmail.com>
2018-01-14 02:04:12 -0500

    server project structure

---
+++ server/src/controllers/root.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = function(app) {
+  app.use('/', router);
+}
+
+router.get('/', function(req, res, next) {
+  res.send('hello world');
+});
