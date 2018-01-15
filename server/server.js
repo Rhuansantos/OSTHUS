96ed1765b6ea3becfdd56bdca1349bc7c425f559
Rhuan Santos <rhuansantosdev@gmail.com>
2018-01-14 19:36:59 -0500

    package json causing an infinity loop
    https://github.com/npm/npm/issues/18477

--- server/server.js
+++ server/server.js
@@ -6,11 +6,12 @@ const fs = require('fs');
 
 const options = {
   key: fs.readFileSync('./encryption/client-key.pem'),
-  cert: fs.readFileSync('./encryption/client-cert.pem')
-}
+  cert: fs.readFileSync('./encryption/client-cert.pem'),
+};
 
 http.createServer(app).listen(4000);
 // https.createServer(options, app).listen(8000); https
 
-const appJS = require('./src/app.js')
+const appJS = require('./src/app.js');
+
 appJS(app);
