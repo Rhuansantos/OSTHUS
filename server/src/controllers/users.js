33da51bd189531ef5a3060b0a14d62708c9ed069
Rhuan Santos <rhuansantosdev@gmail.com>
2018-01-17 15:35:56 -0500

    API user get by id and get all

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -4,19 +4,72 @@ const models = require('../db/models');
 const router = express.Router();
 
 module.exports = (app) => {
-  app.use('/users', router);
+  app.use('/user', router);
 };
 
-router.get('/getById', (req, res) => {
+/**
+ * @api {get} /users/getById/${id}
+ * @apiParam {Number} id Users unique ID.
+ * @apiVersion 0.1.0
+ * @apiName GetUser by id
+ * @apiGroup User
+ *  * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "name": "Rhuan",
+ *       "email": "Santos",
+ *       "Phone": "407-683-6105",
+ *       "createdAt": "2018-01-17T19:50:34.152Z",
+ *       "updatedAt": "2018-01-17T19:50:34.152Z",
+ *       "histories": [
+          {
+          "action": "login",
+          "createdAt": "2018-01-17T19:50:34.188Z",
+          },
+        ]
+ *     }
+ *  * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
+router.get('/getById/:uname', (req, res) => {
   models.users.findAll({
-    include: [{ model: models.histories }],
+    where: { id: req.params.uname },
+    include: [{ model: models.histories, attributes: ['action', 'createdAt'] }],
   }).then((_data) => {
-    res.json(_data);
+    if (_data.length > 0) {
+      res.json(_data);
+    } else {
+      res.send({ error: 'UseNotFound' });
+    }
   });
 });
 
 /**
- * @api {get} /user/:id
+ * @api {get} /users/all
+ * @apiVersion 0.1.0
+ * @apiGroup User
+ *  * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       id: 1,
+ *       "name": "Rhuan",
+ *       "email": "Santos",
+ *       "Phone": "407-683-6105",
+ *       "createdAt": "2018-01-17T19:50:34.152Z",
+ *       "updatedAt": "2018-01-17T19:50:34.152Z",
+ *     }
+ *  * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "Something went wrong"
+ *     }
  */
 router.get('/getAll', (req, res) => {
   models.users.findAll({
