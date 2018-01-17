ff43f60580173dc1aa5c971d51ad85725897b36f
Rhuan Santos <rhuansantosdev@gmail.com>
2018-01-16 23:26:47 -0500

    users controller

---
+++ server/src/controllers/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const models = require('../db/models');
+
+const router = express.Router();
+
+module.exports = (app) => {
+  app.use('/users', router);
+};
+
+router.get('/getById', (req, res) => {
+  models.users.findAll({
+    include: [{ model: models.histories }],
+  }).then((_data) => {
+    res.json(_data);
+  });
+});
+
+router.get('/getAll', (req, res) => {
+  models.users.findAll({
+    include: [{ model: models.histories }],
+  }).then((_data) => {
+    res.json(_data);
+  });
+});
