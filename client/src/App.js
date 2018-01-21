0cef093bdd96ff6ceb036c86dc1a6ba0f02f361d
Rhuan Santos <rhuansantosdev@gmail.com>
2018-01-21 17:05:17 -0500

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import Home from './components/home';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
-class App extends Component {
+const Test = () => (
+  <div>
+    <h2>Test</h2>
+  </div>
+)
+
+const App = () => ({
   render() {
     return (
-      <Home/>
+      <div>
+      <Link to='/test'>Test</Link>
+      
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/test' render={(props) => <Test/>} />
+      </Switch>
+      </div>
     );
   }
-}
+});
 
 export default App;
